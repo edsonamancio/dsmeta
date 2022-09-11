def41381895ba8ab7d5ee21e3da0764f1b0a73bd
Edson Carvalho <edsoncarvalho@gmail.com>
2022-09-11 14:36:16 -0300

    Heroku

--- frontend/src/componests/Header/index.tsx
+++ frontend/src/componests/Header/index.tsx
@@ -8,10 +8,7 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src= {logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
-                <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-                </p>
+               
             </div>
         </header>
 
